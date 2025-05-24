f85cdf376cc04c500be055e94a62b7916c9564dc
lucuinhas <lucarvalhodealmeida@gmail.com>
2025-05-24 00:12:58 -0400

    first commit. has basic homework functionality, no tests, and no complicated description shenanigans

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 init()
 
 function init() {
-    let homeworkFile = fetch("../data/homework.json")
+    let homeworkFile = fetch("data/homework.json")
         .then(response => response.json().then(data => loadHomework(data)))
         .catch(error => console.log(error))
 }
